eeb79f4e24f9e692ef3929b3b67a5d0b67161cb2
Tahminur Rahman <32945811+Tahminur@users.noreply.github.com>
2025-07-02 21:29:01 -0400

    Initial Commit

---
+++ MovieLibraryBrowserTests/MovieLibraryBrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieLibraryBrowserTests.swift
+//  MovieLibraryBrowserTests
+//
+//  Created by Tahminur Rahman on 7/2/25.
+//
+
+import Testing
+@testable import MovieLibraryBrowser
+
+struct MovieLibraryBrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
